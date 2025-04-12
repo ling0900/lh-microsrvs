acee3a99428fad6dd37af162b6bc6b2914ab7043
lh <992610900@qq.com>
2025-04-12 16:53:08 +0800

    redisson注入的小问题。

--- my-redisson-application/src/main/java/com/yourcompany/Api.java
+++ my-redisson-application/src/main/java/com/yourcompany/Api.java
@@ -1,18 +1,18 @@
 package com.yourcompany;
 
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
+   // 这种写法，运行启动类Main会出错。
+   @Autowired
+   Redisson redisson;
 
-    @Autowired
-    Redisson redisson;
 
-    @GetMapping("/a")
-    public String get() {
-        return "Hello";
-    }
+   // 这种写法，运行启动类Main不会出错。
+//   @Autowired
+//   RedissonClient redisson;
 }
