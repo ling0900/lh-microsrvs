f447b0bcf91e9f162e49df8a95bed0733337a8bd
lh <992610900@qq.com>
2025-04-12 13:47:54 +0800

    假设直接用redisson。备份代码。

--- my-redisson-application/src/main/java/com/yourcompany/Api.java
+++ my-redisson-application/src/main/java/com/yourcompany/Api.java
@@ -1,26 +1,12 @@
 package com.yourcompany;
 
-import com.yourcompany.redisson.redismanager.RedissManager;
 import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ComponentScan(value = {
-        "com.yourcompany"
-
-
-})
 public class Api {
-
-//    @Autowired
-//    RedissManager redissManager;
-
-//    @Autowired
-//    RedissonClient redissonClient;
 
     @Autowired
     Redisson redisson;
