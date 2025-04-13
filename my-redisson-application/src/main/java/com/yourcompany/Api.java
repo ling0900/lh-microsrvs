471e08cc5ae18dd5ff4fd0cb2a6a803b21f1f7d9
lh <992610900@qq.com>
2025-04-13 12:37:03 +0800

    去掉引入的redisson starter

--- my-redisson-application/src/main/java/com/yourcompany/Api.java
+++ my-redisson-application/src/main/java/com/yourcompany/Api.java
@@ -1,18 +1,7 @@
 package com.yourcompany;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
-   // 这种写法，运行启动类Main会出错。
-   @Autowired
-   Redisson redisson;
-
-
-   // 这种写法，运行启动类Main不会出错。
-//   @Autowired
-//   RedissonClient redisson;
 }
